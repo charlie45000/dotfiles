059862641df1a0daf153e83ec2138151635753e0
paul giacopelli <paul.giacopelli@insa-cvl.fr>
2019-06-25 23:28:42 +0200

    cleared repo before reupload new dotfiles

--- .i3/shutdown.sh
+++
@@ -1,2 +0,0 @@
-i3-nagbar -t warning -m 'You pressed the shutdown shortcut. do yo realy want to quit?.' -b 'Yes, quit' 'sh ~/.i3/shutdown_def.sh -P now'
-exit 0
--- .i3/shutdown_def.sh
+++
@@ -1,4 +0,0 @@
-i3-msg "[floating] kill; [tiling] kill" 
-shutdown $1 $2
-#sh ~/Documents/tri.sh
-
