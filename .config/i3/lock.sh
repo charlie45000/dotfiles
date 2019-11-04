92f064a66229c87ffb0f47bf7200a4faa4e36f82
charlie45000 <porky45000@gmail.com>
2019-11-05 00:53:53 +0100

    added suspend in shutdown options and updated path and names

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-icon="$HOME/.config/i3/lock.png"
+icon="$I3C/lock.png"
 tmpbg='/tmp/screen.png'
 
-scrot "$tmpbg"
+scrot -o "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -i "$tmpbg"
