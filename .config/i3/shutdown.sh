0c118a6dcb485726dfd405466b3578b49dad7920
paul giacopelli <paul.giacopelli@insa-cvl.fr>
2019-12-26 15:25:13 +0100

    made shutdown script posix-shell-complient

--- .config/i3/shutdown.sh
+++ .config/i3/shutdown.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-answer=$(echo -e "Restart i3\nExit i3\nSuspend\nHibernate\nReboot\nShutdown" | rofi -dmenu -i -p "Shutdown options" -lines 6 -hide-scrollbar -font "Whatever 12" -location 0)
+answer=$(printf "Restart i3\nExit i3\nSuspend\nHibernate\nReboot\nShutdown" | rofi -dmenu -i -p "Shutdown options" -lines 6 -hide-scrollbar -font "Whatever 12" -location 0)
 
-[ "$answer" == "Exit i3" ] && i3-msg exit
-[ "$answer" == "Restart i3" ] && i3-msg restart
-[ "$answer" == "Suspend" ] && $I3C/lock.sh && systemctl suspend
-[ "$answer" == "Hibernate" ] && $I3C/lock.sh && systemctl hibernate
-[ "$answer" == "Reboot" ] && systemctl reboot 
-[ "$answer" == "Shutdown" ] && systemctl poweroff
+#the sleep are here to prevent scrot from tacking a screeshot with rofi in it
+
+[ "$answer" = "Exit i3" ] && i3-msg exit
+[ "$answer" = "Restart i3" ] && i3-msg restart
+[ "$answer" = "Suspend" ] && sleep 0.15 && "$I3C"/lock.sh && systemctl suspend
+[ "$answer" = "Hibernate" ] && sleep 0.15 && "$I3C"/lock.sh && systemctl hibernate
+[ "$answer" = "Reboot" ] && systemctl reboot
+[ "$answer" = "Shutdown" ] && systemctl poweroff
