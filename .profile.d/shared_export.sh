d7537914534456bd587f5a3ae187bb9ad0edf0e5
charlie45000 <porky45000@gmail.com>
2019-12-26 15:34:27 +0100

    changed profile and x session loading

---
+++ .profile.d/shared_export.sh
@@ -0,0 +1,6 @@
+LIBRARY_PATH="${HOME}/.local/lib/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${LIBRARY_PATH%:}"
+export EDITOR="vim"
+export PATH="${PATH}:${HOME}/bin"
+export TERMINAL="urxvt"
+export BROWSER="firefox"
