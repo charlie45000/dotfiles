037483fea7bd1944094e77ff9df4081882dabea7
paul giacopelli <paul.giacopelli@insa-cvl.fr>
2019-12-26 15:11:19 +0100

    changed background setting and added new way of using a workspace (no matter the name)

---
+++ .config/i3/more-wksp.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#[ "$1" = "go" ] && (prompt=$1) || ([ "$1" = "move" ] && (prompt="")) 
+
+wksps=$(i3-msg -t 'get_workspaces' | python -c 'import json;[print(wksp["name"]) for wksp in json.loads(input())];')
+lines=$(echo "$wksps" | wc -l)
+wksp=$(echo "$wksps" | rofi -dmenu -i -p "Chose a workspace to $1 to" -font "Whatever 12" -lines "$lines" -hide-scrollbar -location 0)
+
+[ "$wksp" ] && case "$1" in
+	"go")
+		i3-msg workspace "$wksp"
+		;;
+	"move")
+		i3-msg move container to workspace "$wksp"
+		;;
+	"move-go")
+		i3-msg move container to workspace "$wksp"
+		i3-msg workspace "$wksp"
+		;;
+	*)
+		echo "not a valid command"
+		;;
+esac
