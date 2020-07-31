f6155e65d45a1d4e01900243774e93cd51a5e3be
charlie45000 <porky45000@gmail.com>
2020-07-31 19:07:39 +0200

    updated various files

--- .profile.d/shared_export.sh
+++ .profile.d/shared_export.sh
@@ -20,7 +20,12 @@ LIBRARY_PATH="${HOME}/.local/lib/:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${LIBRARY_PATH%:}"
 export EDITOR="nvim"
 export MC="$HOME/.config/mutt"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
 appendpath "${HOME}/.config/vim/plugged/vim-live-latex-preview/bin"
-appendpath "${HOME}/bin"
+appendpath "${HOME}/.local/bin"
+appendpath "${HOME}/.local/daemons"
+appendpath "${HOME}/scripts"
 echo "$0, $PATH" >> /tmp/profile.tmp
